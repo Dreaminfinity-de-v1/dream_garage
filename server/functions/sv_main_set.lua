d6056893b1c051f8e2169db7541a19c9efb9b228
Grewo <grewo@t-online.de>
2021-10-25 23:40:39 +0200

    Update to modular

--- server/functions/sv_main_set.lua
+++ server/functions/sv_main_set.lua
@@ -56,8 +56,13 @@ function setVehicleInTowingyardByZeroUser()
     local xPlayers = ESX.GetPlayers()
 
     if #xPlayers <= 0 then
-        MySQL.Sync.execute("UPDATE `dream_owned_vehicle` SET `garage_id` = @garage_id WHERE `garage_id` = NULL", {
-            ['@garage_id'] = towingyard_test,
-        })
+
+        for k, v in pairs(Config.DefaultTowingyards) do
+            MySQL.Sync.execute("UPDATE `dream_owned_vehicle` SET `garage_id` = @garage_id WHERE `garage_id` = NULL AND `type` = @type", {
+                
+                ['@type'] = k,
+                ['@garage_id'] = v,
+            })
+        end
     end
 end
